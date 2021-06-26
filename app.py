f66ab05b8c96f1a680aa6f10c5fca71214c75fd8
pmltayamora <pmltayamora@gmail.com>
2021-06-27 04:02:11 +0800

    DATA table functional

--- app.py
+++ app.py
@@ -1,39 +1,15 @@
 from flask import Flask, render_template, request, redirect
-from flask import g
-import os
+from data import *
 import sqlite3
 import time
 
-currentlocation = os.path.dirname(os.path.abspath(__file__))
-
 app = Flask(__name__)
-
-DATABASE = './Login.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
 
 @app.teardown_appcontext
 def close_connect(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
-
-def check_login(Username, Password):
-    user = query_db('SELECT * FROM Users WHERE Username = ? and Password = ?', [Username, Password], one=True)
-    if user is None:
-        return False
-    else:
-        return True
 
 @app.route('/')
 @app.route('/home')
@@ -42,7 +18,7 @@ def home():
 
 @app.route('/login', methods = ['GET'])
 def login1():
-    return render_template('login.html')
+    return render_template('admin_login.html')
 
 @app.route('/login', methods=['POST'])
 def login2():
@@ -51,36 +27,37 @@ def login2():
 
     sqlconnection = sqlite3.Connection(currentlocation + '\Login.db')
     cursor = sqlconnection.cursor()
-    query1 = "SELECT Username, Password FROM Users WHERE Username = '{un}' AND Password = '{pw}'".format(un = UN, pw = PW)
+    query = "SELECT Username, Password FROM Users WHERE Username = '{un}' AND Password = '{pw}'".format(un = UN, pw = PW)
 
-    check_rows = cursor.execute(query1)
-    check_rows = check_rows.fetchall()
+    rows = cursor.execute(query)
+    rows = rows.fetchall()
 
     error = None
 
-    if len(check_rows) == 1:
+    if len(rows) == 1:
         return redirect('/dashboard')
         #render will be changed dashboard still to be edited
     else:
         if check_login(request.form['Username'], request.form['Password']):
-            return redirect('/register')
+            return redirect('/dashboard')
         else:
             error = 'Username and password not recognized'
             time.sleep(1)
-            return render_template('login.html', error=error)
+            return render_template('admin_login.html', error=error)
 
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
+@app.route('/admin_register', methods = ['GET', 'POST'])
+def admin_register():
     if request.method == 'POST':
         rFN = request.form['RFirstname']
         rLN = request.form['RLastname']
         rUN = request.form['RUsername']
         rPW = request.form['RPassword']
-        sqlconnection = sqlite3.Connection(currentlocation + '\Login.db')
+        sqlconnection = sqlite3.Connection(currentlocation + '/Login.db')
         cursor = sqlconnection.cursor()
 
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS Users(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT
         FirstName VARCHAR(20) NOT NULL,
         LastName VARCHAR(20) NOT NULL,
         Username VARCHAR(20) NOT NULL,
@@ -88,18 +65,101 @@ def register():
         ''')
         #makes table named Users if it doesnt exist
 
-        query1 = "INSERT INTO Users VALUES('{f}','{l}','{u}','{p}')".format(f=rFN, l=rLN, u=rUN, p=rPW)
+        query1 = "INSERT INTO Users VALUES(null,'{f}','{l}','{u}','{p}')".format(f=rFN, l=rLN, u=rUN, p=rPW)
         cursor.execute(query1)
         sqlconnection.commit()
         return redirect('/')
 
-    return render_template('register.html')
+    return render_template('admin_register.html')
     #could not make function verifying if username is already taken
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    sqlconnection = sqlite3.connect(currentlocation + '\Login.db')
+    sqlconnection.row_factory = sqlite3.Row
+    cur = sqlconnection.cursor()
+    cur.execute("SELECT * FROM Users")
+    rows = cur.fetchall()
+    return render_template('dashboard.html', rows=rows)
     #very bare no design yet
+
+@app.route('/register', methods=['get','post'])
+def register():
+    if request.method == 'POST':
+        vCT = request.form['VCategory']
+        vLN = request.form['VLastname']
+        vFN = request.form['VFirstname']
+        vMN = request.form['VMiddlename']
+        vCN = request.form['VContactnumber']
+        vEM = request.form['VEmail']
+        vBM = request.form['VBirthmonth']
+        vBD = request.form['VBirthdate']
+        vBY = request.form['VBirthyear']
+        vAG = request.form['VAge']
+        vGN = request.form['VGender']
+        vCS = request.form['VCivilstatus']
+        vRG = request.form['VRegion']
+        vPR = request.form['VProvince']
+        vCY = request.form['VCity']
+        vBR = request.form['VBarangay']
+        vAD = request.form['VAddress']
+        vPS = request.form['VPregnancystatus']
+        vCI = request.form['VCovidinteraction']
+        vAL = request.form['VAllergies']
+        vAL2 = request.form['VAllergies2']
+        vCM = request.form['VComorbidity']
+        vSL = request.form['VSelection']
+        vDG = request.form['VDiagnosis']
+        vCL = request.form['VClassification']
+        vCD = request.form['VCoviddate']
+        vCO = request.form['VConsent']
+        vUN = request.form['VUsername']
+        vPW = request.form['VPassword']
+
+        sqlconnection = sqlite3.Connection(currentlocation + '\Form.db')
+        cursor = sqlconnection.cursor()
+
+        cursor.execute('''
+                    CREATE TABLE IF NOT EXISTS DATA(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    category VARCHAR(20) NOT NULL,
+                    last_name VARCHAR(20) NOT NULL,
+                    first_name VARCHAR(20) NOT NULL,
+                    mid_name VARCHAR(20) NOT NULL,
+                    contact_num TEXT NOT NULL,
+                    email TEXT NOT NULL,
+                    birthmonth VARCHAR(20) NOT NULL,
+                    birthdate INTEGER NOT NULL,
+                    birthyear INTEGER NOT NULL,
+                    age INTEGER NOT NULL,
+                    gender VARCHAR(20) NOT NULL,
+                    civil_status VARCHAR(20) NOT NULL,
+                    region VARCHAR(20) NOT NULL,
+                    province VARCHAR(20) NOT NULL,
+                    city VARCHAR(20) NOT NULL,
+                    barangay TEXT NOT NULL,
+                    address TEXT NOT NULL,
+                    pregnancy_status TEXT NOT NULL,
+                    covid_interaction TEXT NOT NULL,
+                    allergy TEXT NOT NULL,
+                    allergy_list VARCHAR(100),
+                    comorbidity TEXT NOT NULL,
+                    selection VARCHAR(100),
+                    diagnosis TEXT NOT NULL,
+                    classification VARCHAR(100),
+                    covid_date TEXT NOT NULL,
+                    consent TEXT NOT NULL,
+                    username VARCHAR(20) NOT NULL,
+                    password VARCHAR(20) NOT NULL);
+                    ''')
+
+        query1 = "INSERT INTO DATA VALUES(null,'{ct}','{ln}','{fn}','{mn}','{cn}','{em}','{bm}','{bd}','{by}','{ag}','{gn}','{cs}','{rg}','{pr}','{cy}','{br}','{ad}','{ps}','{ci}','{al}','{al2}','{cm}','{sl}','{dg}','{cl}','{cd}','{co}','{un}','{pw}')".format(ct=vCT, ln=vLN, fn=vFN, mn=vMN, cn=vCN, em=vEM, bm=vBM, bd=vBD, by=vBY, ag=vAG, gn=vGN, cs=vCS, rg=vRG, pr=vPR, cy=vCY, br=vBR, ad=vAD, ps=vPS, ci=vCI, al=vAL, al2=vAL2, cm=vCM, sl=vSL, dg=vDG, cl=vCL, cd=vCD, co=vCO, un=vUN, pw=vPW)
+        cursor.execute(query1)
+
+        sqlconnection.commit()
+        return redirect('/home')
+
+    return render_template('register.html')
 
 @app.route('/about')
 def about():
